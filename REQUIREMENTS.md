0b7e7d82b0f5091ed73a95bec812c54601892d61
Igor de Almeida <igoor1205@gmail.com>
2021-01-18 15:17:09 -0300

    :construction: Create base layout

---
+++ REQUIREMENTS.md
@@ -0,0 +1,19 @@
+# Pomodoro Features
+
+### _#1_ Técnica Pomodoro
+
+> Permite ao usuário realizar a técnica de pomodoro, que consiste em dividir o seu horário de trabalho em ciclos de **X** minutos e em seguida **Y** minutos, sendo **X** o tempo de trabalho e **Y** o tempo de descanso.
+
+#### Requisitos funcionais
+
+- [ ] O usuário deve poder ver um tempo X, sendo X a sessão de trabalho ou descanso.
+- [ ] O usuário deve poder iniciar, pausar e resetar o cronômetro.
+- [ ] O usuário deve poder digitar quanto minutos irão durar os tempos X e Y.
+
+#### Regras de negócio
+
+- [ ] O usuário não pode pausar o cronômetro durante a sessão de descanso.
+- [ ] `5 < X <= 60`
+- [ ] `1 < Y <= 20`
+
+---
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
 
+import Logo from './assets/logo.png'
+import Play from './assets/play.png'
+
+import './App.scss'
+
 const App: React.FC = () => {
-  return <div>Hello World</div>
+  return (
+    <main id="main-container">
+      <div className="content">
+        <div id="logo-container">
+          <img src={Logo} alt="Logo" />
+        </div>
+
+        <div id="progress-container">
+          <div className="progress">25:00</div>
+        </div>
+
+        <div id="actions-container">
+          <button type="button" className="action-container">
+            <img src={Play} alt="Play icon" />
+          </button>
+        </div>
+
+        <div id="sliders-container">
+          <div className="slider-container">
+            <label htmlFor="working-input">
+              Trabalho
+              <input id="working-input" type="text" />
+            </label>
+          </div>
+          <div className="slider-container">
+            <label htmlFor="break-input">
+              Descanso
+              <input id="break-input" type="text" />
+            </label>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
